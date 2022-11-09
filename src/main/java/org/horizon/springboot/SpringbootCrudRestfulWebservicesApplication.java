1ccd36e4a21faa6bfaac250da8c395f1239a8a2e
Wassef911 <1.wassef911@gmail.com>
2022-11-09 20:58:44 +0100

    choose port

--- src/main/java/org/horizon/springboot/SpringbootCrudRestfulWebservicesApplication.java
+++ src/main/java/org/horizon/springboot/SpringbootCrudRestfulWebservicesApplication.java
@@ -1,5 +1,7 @@
 package org.horizon.springboot;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootCrudRestfulWebservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootCrudRestfulWebservicesApplication.class, args);
+		SpringApplication app = new SpringApplication(SpringbootCrudRestfulWebservicesApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8000"));
+		app.run(args);
 	}
 
 }
