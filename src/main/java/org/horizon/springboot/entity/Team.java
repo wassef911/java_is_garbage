beef2578af3a675884dbc35b94f1102355267f6d
Wassef911 <1.wassef911@gmail.com>
2022-11-10 16:54:43 +0100

    progress

--- src/main/java/org/horizon/springboot/entity/Team.java
+++ src/main/java/org/horizon/springboot/entity/Team.java
@@ -12,7 +12,6 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.horizon.springboot.entity.Player;
 
 import javax.persistence.OneToMany;
 
